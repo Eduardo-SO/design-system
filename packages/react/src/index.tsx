9889bc5fb1956e6e9cac8decfd3583a63406ada2
Eduardo S.O <eduardo.sool07@gmail.com>
2023-02-03 01:21:45 -0300

    Add Button stories

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,36 @@
+import { ComponentProps } from 'react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
-  fontFamily: '$code',
-  backgroundColor: '$izuka300',
-  borderRadius: '$md',
-  padding: '$4',
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$izuka500',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+  transition: 'all 0.2s ease',
+
+  '&:hover': {
+    backgroundColor: '$izuka700',
+  },
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello World</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
